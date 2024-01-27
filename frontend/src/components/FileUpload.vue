87ffa45e8d5bbe223e3e3786677108c615b5a834
TapeMate <admin@tapemate.onmicrosoft.com>
2024-01-27 07:50:17 +0100

    cleanup and creating first version of file upload component.

---
+++ frontend/src/components/FileUpload.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form action="#">
+      <h3>csv upload</h3>
+      <label for="csv">CSV-Datei hochladen</label>
+      <input id="csv" type="file" />
+      <button>Upload</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FileUpload",
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-bottom: 1px solid black;
+}
+
+label {
+  padding: 0.5rem;
+}
+
+button {
+  padding: 0.5rem 1rem;
+  margin: 1rem;
+}
+</style>
