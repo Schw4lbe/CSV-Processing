87b8a786933c83254ee3c12bf392658f57743f88
TapeMate <admin@tapemate.onmicrosoft.com>
2024-01-29 18:43:35 +0100

    installed vuetify to display data table.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="home">
+  <div class="home-wrapper">
     <FileUpload />
+    <DataTable />
   </div>
 </template>
 
 <script>
-import FileUpload from "@/components/FileUpload.vue";
+import { defineComponent } from "vue";
 
-export default {
+// Components
+import FileUpload from "../components/FileUpload.vue";
+import DataTable from "../components/DataTable.vue";
+
+export default defineComponent({
   name: "HomeView",
+
   components: {
     FileUpload,
+    DataTable,
   },
-};
+});
 </script>
