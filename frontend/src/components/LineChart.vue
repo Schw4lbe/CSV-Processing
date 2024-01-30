6d988e071a52a1533f61ce4a1dd186272d3a41d5
TapeMate <admin@tapemate.onmicrosoft.com>
2024-01-30 15:38:50 +0100

    minor changes.

--- frontend/src/components/LineChart.vue
+++ frontend/src/components/LineChart.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <area-chart
-      :data="{ '2017-01-01 00:00:00 -0800': 2, '2017-01-01 00:01:00 -0800': 5 }"
-    ></area-chart>
+    <pie-chart
+      :data="[
+        ['Blueberry', 44],
+        ['Strawberry', 23],
+      ]"
+    ></pie-chart>
   </div>
 </template>
 
