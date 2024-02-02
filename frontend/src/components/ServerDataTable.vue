fe910dc936f01420a5641fc215fe7a8f884ecdc7
TapeMate <admin@tapemate.onmicrosoft.com>
2024-02-02 11:23:29 +0100

    added more table options from vuetify documentation.

--- frontend/src/components/ServerDataTable.vue
+++ frontend/src/components/ServerDataTable.vue
@@ -8,9 +8,101 @@
     @update:options="loadItems"
     class="elevation-1"
   >
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>My CRUD</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ props }">
+            <v-btn color="primary" dark class="mb-2" v-bind="props">
+              New Item
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Dessert name"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.calories"
+                      label="Calories"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.fat"
+                      label="Fat (g)"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.carbs"
+                      label="Carbs (g)"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.protein"
+                      label="Protein (g)"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="close">
+                Cancel
+              </v-btn>
+              <v-btn color="blue-darken-1" variant="text" @click="save">
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5"
+              >Are you sure you want to delete this item?</v-card-title
+            >
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="closeDelete"
+                >Cancel</v-btn
+              >
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                @click="deleteItemConfirm"
+                >OK</v-btn
+              >
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+
     <template v-slot:[`item.actions`]="{ item }">
       <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
       <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+    </template>
+
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="loadItems"> Reset </v-btn>
     </template>
   </v-data-table-server>
 </template>
@@ -25,6 +117,9 @@ export default {
     totalItems: 0,
     itemsPerPage: 5,
     loading: false,
+
+    dialog: false,
+    dialogDelete: false,
   }),
 
   computed: {
@@ -41,6 +136,13 @@ export default {
         this.loadItems();
       }
     },
+
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
   },
 
   methods: {
@@ -49,11 +151,31 @@ export default {
     editItem(item) {
       // Implement your logic for editing an item
       console.log("Edit item:", item);
+      this.dialog = true;
     },
 
     deleteItem(item) {
       // Implement your logic for deleting an item
       console.log("Delete item:", item);
+      this.dialogDelete = true;
+    },
+
+    close() {
+      this.dialog = false;
+      console.log("close");
+      // this.$nextTick(() => {
+      //   this.editedItem = Object.assign({}, this.defaultItem)
+      //   this.editedIndex = -1
+      // })
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      console.log("closeDelete");
+      // this.$nextTick(() => {
+      //   this.editedItem = Object.assign({}, this.defaultItem)
+      //   this.editedIndex = -1
+      // })
     },
 
     async loadItems({ page, itemsPerPage, sortBy } = {}) {
@@ -103,14 +225,14 @@ export default {
       keys.forEach((key) => {
         const newObj = {};
         newObj.title = key;
-        newObj.value = key;
+        newObj.key = key;
         this.headers.push(newObj);
       });
 
       // add actions column
       this.headers.push({
-        title: "Actions",
-        value: "actions",
+        title: "Aktionen",
+        key: "actions",
         sortable: false,
       });
     },
