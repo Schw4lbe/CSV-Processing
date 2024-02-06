2aea2e11d1b83b03279c03eb2b6c6891d4b0dd33
TapeMate <admin@tapemate.onmicrosoft.com>
2024-02-07 00:50:25 +0100

    refactoring DataChart comp and added reset function. outsourced styling in partial. added info into docu.

--- documentation.md
+++ documentation.md
@@ -121,6 +121,256 @@ async onSubmit() {
     },
 ```
 
+### DataChart.vue
+
+> Die DataChart Komponente agiert teilweise als Navigationsleiste. Sie lässt sich auf klappen um Diagramme an zu zeigen. Select Inputs ermöglichen die Grafische Darstellung der Tabellen Spalten. Rechts am Rand befindet sich der Beenden Button zum Schließen der Anwendung. Es sind 2 Diagramme dargestellt. Beide können unabhängig voneinander Daten anzeigen, welche sich auf alle aktuell dargestellten Zeilen beziehen. Der Beenden Button öffnet einen Dialog. Bei Bestätigung werden Session Variablen resettet / gelöscht. Ebenso wird ein Request an das Backend zum Drop des SQL Tables geschickt. Durch Löschung des tableName gelangt der Nutzer wieder zum FileUpload Screen.
+
+##### TEMPLATE
+
+> Vorweg der Beenden Dialog samt Auswahl Buttons und Events für Session Exit.
+
+```html
+  // Nur angezeigt wenn Beenden Button geklickt wurde und noch ein tableName existiert
+  <div v-if="exitConfirmPending && getTableName" class="exit-confirm">
+    //Code
+      <div class="btn-confirm-container">
+        // Buttons mit Events zur Bestätigung
+        <v-btn @click="cancelExit" class="btn-confirm-exit">Abbrechen</v-btn
+        ><v-btn @click="confirmExit" class="btn-confirm-exit">Beenden</v-btn>
+      </div>
+    </div>
+  </div>
+```
+
+> Im Chart Wrapper befindet sich der toggle Container zum ein- und ausblenden der Diagramme sowie der Beenden Button.
+
+```html
+<div v-if="getTableName" class="chart-wrapper">
+  <div class="chart-control">
+    // Steuert Sichtbarkeit der Diagramme und animiert Pfeil und Text
+    <div @click="toggleCharts" class="chart-header chart-toggle">
+      <span
+        :class="{
+            'arrow-right': !chartsVisible,
+            'arrow-down': chartsVisible,
+          }"
+      ></span
+      ><span class="cart-description">Grafische Auswertung</span>
+    </div>
+    // Beenden Button zum öffnen des Dialogs
+    <button @click="handleExit" class="btn-exit">beenden</button>
+  </div>
+</div>
+```
+
+> Die beiden Dropdowns zur Auswahl der Diagrammdaten besitzen beide ein Event, welches bei Änderungen neue Daten importiert. Mittels V-Model sind diese mit einer property verknüpft. Die Optionen werden per v-for anhand der chartCategories erzeugt (hierzu nachfolgend mehr).
+
+```html
+<div class="select">
+  <label for="select1">Kuchen Diagramm</label>
+  <select
+    @change="updateSelect"
+    v-model="selectedChartCat1"
+    name="select1"
+    id="select1"
+  >
+    // chartCategorie bekommt daten von dataTable fetch und wird vorab gefiltert
+    <option v-for="(item, index) in chartCategories" :key="index" :value="item">
+      {{ item }}
+    </option>
+  </select>
+</div>
+
+<div class="select">
+  <label for="select2">Balken Diagramm</label>
+  <select>
+    // Gleicher Aufbau hier
+    <option>// Gleicher Aufbau hier</option>
+  </select>
+</div>
+```
+
+> Die beiden Diagramme werden mittels ChartKick Librarie eingebunden. Der Suffix Wert ermöglich die Darstellung in %.
+
+```html
+<pie-chart :data="pieChartData" suffix="%" />
+<column-chart :data="columnChartData" suffix="%"></column-chart>
+```
+
+##### SCRIPT
+
+> Data Values zur Komponentensteuerung
+
+```js
+ data() {
+    return {
+      // toggle Indikator
+      chartsVisible: false,
+
+      // Caching von Diagrammdaten
+      chartCategories: [],
+      pieChartData: [],
+      columnChartData: [],
+
+      // Standardwerte für Diagramme
+      selectedChartCat1: "Geschlecht",
+      selectedChartCat2: "Grammatur",
+
+      // Bool für Beenden Dialog
+      exitConfirmPending: false,
+    };
+  },
+```
+
+> Ein Watcher achtet auf neue Daten im chartData State. Daten werden danach zur Aufbereitung weitergereicht. Beide Diagramme bekommen den Default Wert zugewiesen.
+
+```js
+watch: {
+    getChartData(newVal, oldVal) {
+      // Guard zur vermeidung von Fehlern bei Session Exit ohne local Storage Daten
+      if (newVal === null || newVal === undefined) {
+        return;
+      }
+      // Wenn ein neuer Wert eingeht, Weitergabe zur Aufbereitung und Defaults setzen
+      if (newVal !== oldVal) {
+        this.setChartCategories(newVal);
+        this.setChartData(newVal, this.selectedChartCat1, "select1");
+        this.setChartData(newVal, this.selectedChartCat2, "select2");
+      }
+    },
+  },
+```
+
+> setChartCategories lädt aufbereitete Kategorien in den Cache.
+
+```js
+    setChartCategories(data) {
+      const categories = [];
+      // Filtert gewisse Kategorien heraus
+      Object.keys(data[0]).forEach((key) => {
+        if (
+          key === "id" ||
+          key === "Hauptartikelnr" ||
+          key === "Artikelname" ||
+          key === "Beschreibung"
+        ) {
+          return;
+        }
+        categories.push(key);
+      });
+      // setzt Cache Wert für Rendering
+      this.chartCategories = categories;
+    },
+```
+
+> setChartData bereitet die empfangenen Daten aus dem State zur Darstellung in den Diagrammen auf.
+
+- data = Daten aller Kategorien
+- cat = Kategorie welche aufbereitet werden soll
+- id = id des Diagramms welches die Daten empfangen soll
+
+```js
+    setChartData(data, cat, id) {
+      // Ermittlung Anzahl an Datensätzen
+      const amount = data.length;
+      // Zusammenfassung aller Datensätze in Key: Value Paare nach Kategorie mit summierter Anzahl
+      const itemsObj = data.reduce((acc, el) => {
+        acc[el[cat]] = (acc[el[cat]] || 0) + 1;
+        return acc;
+      }, {});
+
+      // In Object mit Key empty String durch "k.A." für keine Angabe ersetzen
+      if (Object.hasOwnProperty.call(itemsObj, "")) {
+        itemsObj["k.A."] = itemsObj[""];
+        delete itemsObj[""];
+      }
+
+      // Umwandlung von Stück zu Prozent (Eintrag Anzahl / Datensatz Anzahl) * 100 gerundet auf .00
+      Object.keys(itemsObj).forEach((key) => {
+        itemsObj[key] = ((itemsObj[key] / amount) * 100).toFixed(2);
+      });
+
+      // Zuweisung der Daten anhand ID
+      if (id === "select1") {
+        this.pieChartData = Object.entries(itemsObj);
+        this.selectedChartCat1;
+      } else if (id === "select2") {
+        this.columnChartData = Object.entries(itemsObj);
+      }
+    },
+```
+
+> Bei Auswahl anderer Kategorie in Select Input reagiert das updateSelect Event.
+
+```js
+    updateSelect(e) {
+      // Abfangen der ID via Event Target
+      if (e.target.id === "select1") {
+        this.setChartData(
+          this.getChartData,
+          this.selectedChartCat1,
+          e.target.id
+        );
+      } else if (e.target.id === "select2") {
+        // das gleiche für das 2. Diagramm
+      }
+    },
+```
+
+> Weitere Komponenten Methoden.
+
+```js
+    // blendet Diagramme ein und aus
+    toggleCharts() {
+      this.chartsVisible = !this.chartsVisible;
+    },
+    // Zeigt Beenden Dialog an
+    handleExit() {
+      this.exitConfirmPending = true;
+    },
+    // Bricht Beenden Prozess ab
+    cancelExit() {
+      this.exitConfirmPending = false;
+    },
+```
+
+> confirmExit Beendet den Bearbeitungsprozess, Setzt Session data zurück / löscht state values. Zudem wird via action und service API ein Request ans Backend zur Löschung des Data Tables in SQL angestoßen.
+
+```js
+    async confirmExit() {
+      const tableName = this.getTableName;
+      try {
+        const response = await this.dropTable(tableName);
+        if (response && response.success) {
+          // Session Data Reset
+          this.unsetSessionData();
+          // Erfolgsnachricht nach Beenden
+          this.setSuccessCode("FES99");
+        }
+      } catch (error) {
+        console.error("Error in confirmExit method:", error);
+        throw error;
+      } finally {
+        // Reset der Cache variablen
+        this.resetChartData();
+      }
+    },
+```
+
+> resetChartData setzt bei Verlassen der Bearbeitung alle cached Values zurück.
+
+```js
+    resetChartData() {
+      this.chartsVisible = false;
+      this.chartCategories = [];
+      this.pieChartData = [];
+      this.columnChartData = [];
+      this.selectedChartCat1 = "Geschlecht";
+      this.selectedChartCat2 = "Grammatur";
+      this.exitConfirmPending = false;
+    },
+```
+
 ### FileExport.vue
 
 > Komponente zur Darstellung eines Buttons zum CSV Download und zur finalen Abfrage vor dem Download. On Button klick öffnet einen Dialog zur Bestätigung.
--- frontend/src/components/DataChart.vue
+++ frontend/src/components/DataChart.vue
@@ -1,75 +1,77 @@
 <template>
-  <!-- exit confirm dialog -->
-  <div v-if="exitConfirmPending && getTableName" class="exit-confirm">
-    <div class="confirm-container">
-      <p class="confirm-info">
-        Sind Sie sicher, dass Sie die Bearbeitung beenden möchten?
-      </p>
-      <p class="confirm-info">
-        Alle nicht exportierten Änderungen gehen verloren!
-      </p>
-      <div class="btn-confirm-container">
-        <v-btn @click="cancelExit" class="btn-confirm-exit">Abbrechen</v-btn
-        ><v-btn @click="confirmExit" class="btn-confirm-exit">Beenden</v-btn>
-      </div>
-    </div>
-  </div>
-
-  <!-- data chart container -->
-  <div v-if="getTableName" class="chart-wrapper">
-    <div class="chart-control">
-      <div @click="toggleDiv2Visibility" class="chart-header div1">
-        <span
-          :class="{
-            'arrow-right': !isDiv2Visible,
-            'arrow-down': isDiv2Visible,
-          }"
-        ></span
-        ><span class="cart-description">Grafische Auswertung</span>
-      </div>
-      <button @click="handleExit" class="btn-exit">beenden</button>
-    </div>
-    <div v-if="isDiv2Visible" class="chart-container div2">
-      <div class="chart-select-container">
-        <div class="select-container">
-          <div class="select">
-            <label for="select1">Select Pie Chart</label>
-            <select
-              @change="updateSelect"
-              v-model="selectedChartCat1"
-              name="select1"
-              id="select1"
-            >
-              <option
-                v-for="(item, index) in chartCategories"
-                :key="index"
-                :value="item"
-              >
-                {{ item }}
-              </option>
-            </select>
-          </div>
-          <div class="select">
-            <label for="select2">Select Col Chart</label>
-            <select
-              @change="updateSelect"
-              v-model="selectedChartCat2"
-              name="select2"
-              id="select2"
-            >
-              <option
-                v-for="(item, index) in chartCategories"
-                :key="index"
-                :value="item"
-              >
-                {{ item }}
-              </option>
-            </select>
-          </div>
+  <div class="chart-main-wrapper">
+    <!-- exit confirm dialog -->
+    <div v-if="exitConfirmPending && getTableName" class="exit-confirm">
+      <div class="confirm-container">
+        <p class="confirm-info">
+          Sind Sie sicher, dass Sie die Bearbeitung beenden möchten?
+        </p>
+        <p class="confirm-info">
+          Alle nicht exportierten Änderungen gehen verloren!
+        </p>
+        <div class="btn-confirm-container">
+          <v-btn @click="cancelExit" class="btn-confirm-exit">Abbrechen</v-btn
+          ><v-btn @click="confirmExit" class="btn-confirm-exit">Beenden</v-btn>
         </div>
-        <pie-chart :data="pieChartData" suffix="%" />
       </div>
-      <column-chart :data="columnChartData" suffix="%"></column-chart>
+    </div>
+
+    <!-- data chart container -->
+    <div v-if="getTableName" class="chart-wrapper">
+      <div class="chart-control">
+        <div @click="toggleCharts" class="chart-header chart-toggle">
+          <span
+            :class="{
+              'arrow-right': !chartsVisible,
+              'arrow-down': chartsVisible,
+            }"
+          ></span
+          ><span class="cart-description">Grafische Auswertung</span>
+        </div>
+        <button @click="handleExit" class="btn-exit">beenden</button>
+      </div>
+      <div v-if="chartsVisible" class="chart-container chart-toggle-container">
+        <div class="chart-select-container">
+          <div class="select-container">
+            <div class="select">
+              <label for="select1">Kuchen Diagramm</label>
+              <select
+                @change="updateSelect"
+                v-model="selectedChartCat1"
+                name="select1"
+                id="select1"
+              >
+                <option
+                  v-for="(item, index) in chartCategories"
+                  :key="index"
+                  :value="item"
+                >
+                  {{ item }}
+                </option>
+              </select>
+            </div>
+            <div class="select">
+              <label for="select2">Balken Diagramm</label>
+              <select
+                @change="updateSelect"
+                v-model="selectedChartCat2"
+                name="select2"
+                id="select2"
+              >
+                <option
+                  v-for="(item, index) in chartCategories"
+                  :key="index"
+                  :value="item"
+                >
+                  {{ item }}
+                </option>
+              </select>
+            </div>
+          </div>
+          <pie-chart :data="pieChartData" suffix="%" />
+        </div>
+        <column-chart :data="columnChartData" suffix="%"></column-chart>
+      </div>
     </div>
   </div>
 </template>
@@ -82,19 +84,15 @@ export default {
 
   data() {
     return {
-      // ui controll:
-      isDiv2Visible: false,
+      chartsVisible: false,
 
       chartCategories: [],
       pieChartData: [],
       columnChartData: [],
 
-      // default values for pre select charts
-      // will be overwritten uppon select something different
       selectedChartCat1: "Geschlecht",
       selectedChartCat2: "Grammatur",
 
-      // exit confirm modal
       exitConfirmPending: false,
     };
   },
@@ -105,7 +103,6 @@ export default {
 
   watch: {
     getChartData(newVal, oldVal) {
-      // guard to prevent error on local storage variable remove on exit
       if (newVal === null || newVal === undefined) {
         return;
       }
@@ -122,34 +119,6 @@ export default {
 
     ...mapMutations(["unsetSessionData", "setSuccessCode"]),
 
-    toggleDiv2Visibility() {
-      this.isDiv2Visible = !this.isDiv2Visible;
-    },
-
-    handleExit() {
-      this.exitConfirmPending = true;
-    },
-
-    async confirmExit() {
-      const tableName = this.getTableName;
-      try {
-        const response = await this.dropTable(tableName);
-        if (response && response.success) {
-          this.unsetSessionData();
-          this.setSuccessCode("FES99");
-        }
-      } catch (error) {
-        console.error("Error in confirmExit method:", error);
-        throw error;
-      } finally {
-        this.exitConfirmPending = false;
-      }
-    },
-
-    cancelExit() {
-      this.exitConfirmPending = false;
-    },
-
     setChartCategories(data) {
       const categories = [];
       Object.keys(data[0]).forEach((key) => {
@@ -164,22 +133,6 @@ export default {
         categories.push(key);
       });
       this.chartCategories = categories;
-    },
-
-    updateSelect(e) {
-      if (e.target.id === "select1") {
-        this.setChartData(
-          this.getChartData,
-          this.selectedChartCat1,
-          e.target.id
-        );
-      } else if (e.target.id === "select2") {
-        this.setChartData(
-          this.getChartData,
-          this.selectedChartCat2,
-          e.target.id
-        );
-      }
     },
 
     setChartData(data, cat, id) {
@@ -205,194 +158,60 @@ export default {
         this.columnChartData = Object.entries(itemsObj);
       }
     },
+
+    updateSelect(e) {
+      if (e.target.id === "select1") {
+        this.setChartData(
+          this.getChartData,
+          this.selectedChartCat1,
+          e.target.id
+        );
+      } else if (e.target.id === "select2") {
+        this.setChartData(
+          this.getChartData,
+          this.selectedChartCat2,
+          e.target.id
+        );
+      }
+    },
+
+    toggleCharts() {
+      this.chartsVisible = !this.chartsVisible;
+    },
+
+    handleExit() {
+      this.exitConfirmPending = true;
+    },
+
+    cancelExit() {
+      this.exitConfirmPending = false;
+    },
+
+    async confirmExit() {
+      const tableName = this.getTableName;
+      try {
+        const response = await this.dropTable(tableName);
+        if (response && response.success) {
+          this.unsetSessionData();
+          this.setSuccessCode("FES99");
+        }
+      } catch (error) {
+        console.error("Error in confirmExit method:", error);
+        throw error;
+      } finally {
+        this.resetChartData();
+      }
+    },
+
+    resetChartData() {
+      this.chartsVisible = false;
+      this.chartCategories = [];
+      this.pieChartData = [];
+      this.columnChartData = [];
+      this.selectedChartCat1 = "Geschlecht";
+      this.selectedChartCat2 = "Grammatur";
+      this.exitConfirmPending = false;
+    },
   },
 };
 </script>
-
-<style scoped>
-.exit-confirm {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  background: rgba(255, 255, 255, 0.3);
-  backdrop-filter: blur(10px);
-  z-index: 1;
-}
-
-.confirm-container {
-  padding: 1rem;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  background: #222;
-  border-radius: 5px;
-}
-
-.confirm-info {
-  color: #eee;
-  padding: 0.5rem;
-  font-size: 1.25rem;
-  font-weight: 500;
-}
-
-.btn-confirm-exit {
-  width: 170px;
-  padding: 0.5rem 1rem;
-  margin: 0.5rem;
-  text-transform: uppercase;
-  background: #222;
-  color: #2194f0;
-  font-size: 1.1rem;
-  transition: all 0.3s;
-  box-shadow: none;
-}
-
-.chart-control {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem;
-}
-
-.btn-exit {
-  padding: 0.5rem;
-  text-align: center;
-  text-transform: uppercase;
-  width: 100px;
-  background: rgba(255, 99, 71, 0.7);
-  color: #222;
-  transition: all 0.3s;
-  font-size: 1.1rem;
-}
-
-.btn-exit:hover {
-  background: tomato;
-  color: white;
-}
-
-.div1,
-.div2 {
-  cursor: pointer;
-}
-
-/* Basic arrow using borders */
-.arrow-right,
-.arrow-down {
-  display: inline-block;
-  margin-right: 5px;
-  border: solid white;
-  border-width: 0 3px 3px 0;
-  padding: 5px;
-  transition: all 0.3s;
-}
-
-.arrow-right {
-  transform: rotate(-45deg);
-  -webkit-transform: rotate(-45deg);
-  border: solid #2194f0;
-  border-width: 0 3px 3px 0;
-  animation: pulse 2s ease-in-out infinite;
-}
-
-.arrow-right + .cart-description {
-  color: white;
-  font-size: 1.1rem;
-  letter-spacing: 3px;
-  transition: all 0.2s;
-}
-
-.arrow-right + .cart-description::after {
-  content: " einblenden";
-}
-
-@keyframes pulse {
-  0% {
-    border: solid #2194f0;
-    border-width: 0 3px 3px 0;
-    padding: 4px;
-  }
-  20% {
-    border: solid white;
-    border-width: 0 3px 3px 0;
-
-    padding: 5px;
-  }
-  80% {
-    border: solid white;
-
-    border-width: 0 3px 3px 0;
-    padding: 5px;
-  }
-  100% {
-    border: solid #2194f0;
-    border-width: 0 3px 3px 0;
-    padding: 4px;
-  }
-}
-
-.arrow-down {
-  transform: rotate(45deg);
-  -webkit-transform: rotate(45deg);
-}
-
-.chart-header {
-  padding: 0.5rem;
-  text-transform: uppercase;
-}
-
-.cart-description {
-  padding: 0.5rem 1rem;
-  font-size: 1.1rem;
-  transition: all 0.2s;
-  color: #2194f0;
-}
-
-.cart-description::after {
-  content: " ausblenden";
-}
-
-.chart-container {
-  width: 100%;
-  padding: 1rem;
-  padding-bottom: 2rem;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  background: #d3d3d3;
-}
-.chart-select-container {
-  display: flex;
-}
-
-.select-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  padding: 1rem;
-  border-right: 1px solid #222;
-}
-
-label {
-  color: #2194f0;
-
-  text-transform: uppercase;
-  margin-top: 2rem;
-  font-size: 1.1rem;
-}
-
-#select1,
-#select2 {
-  padding: 0.5rem 1rem;
-  margin: 0 2rem 2rem 2rem;
-  color: white;
-  background: #222;
-  text-align: center;
-}
-</style>
