e2dbc8ec3c981fa7d4eedcf0df89db3ad3c82d6b
TapeMate <admin@tapemate.onmicrosoft.com>
2024-02-10 19:00:42 +0100

    created .env variable files to be able to switch between dev and production. updated dbh class to decide automatic between dev and prod.

--- backend/classes/dbh.class.php
+++ backend/classes/dbh.class.php
@@ -5,12 +5,14 @@ class Dbh
     {
         try {
             $username = "root";
-            $password = "";
-            $dbh = new PDO("mysql:host=localhost;dbname=csv-processing", $username, $password);
+            $password = "test123!";
+            // Dynamically set the host based on an environment variable or other condition
+            $host = getenv('DOCKER_ENV') ? 'db' : 'localhost';
+            $dbh = new PDO("mysql:host={$host};dbname=csv-processing;charset=utf8mb4", $username, $password);
             return $dbh;
         } catch (PDOException $e) {
             // $e parameter not in output for security reasons
-            // should be logged into a separate file
+            // should be logged into a separate file on separate directory
 
             header('Content-Type: application/json');
             echo json_encode(['error' => 'Database connection error']);
